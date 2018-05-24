529b2260f94549bf1a82aa80fe37251ff00d5d7d
SLimBook <30815731+slimlime@users.noreply.github.com>
2018-05-24 15:19:19 +1000

    Added newly built Vue app

---
+++ precache-manifest.c651558420bb8017a74600c6ed755dfd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "52a4e8db90e498ee742c72ad8691ba62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792f3fff70f35293ff65",
+    "url": "/js/vendors~app.792f3fff.js"
+  },
+  {
+    "revision": "a6f66ca308b6f31e5b2d",
+    "url": "/js/app.a6f66ca3.js"
+  },
+  {
+    "revision": "f3c0271e2279f2bdeea02ddc3aa03dc5",
+    "url": "/img/testimonials-2.f3c0271e.jpg"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/img/bg-masthead.20cbd480.jpg"
+  },
+  {
+    "revision": "828e0935e45611b23593d139bd1984f1",
+    "url": "/img/testimonials-1.828e0935.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dbfff7f304afb1dc5bbd81a3ddba1cb0",
+    "url": "/img/bg-showcase-1.dbfff7f3.jpg"
+  },
+  {
+    "revision": "37b5a1e70d28e0251d4dbe331de3db29",
+    "url": "/img/bg-showcase-2.37b5a1e7.jpg"
+  },
+  {
+    "revision": "15393c5a215a67b0f4bbc4d4ed55f12c",
+    "url": "/img/testimonials-3.15393c5a.jpg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "792f3fff70f35293ff65",
+    "url": "/css/vendors~app.0.220e5520.css"
+  },
+  {
+    "revision": "a6f66ca308b6f31e5b2d",
+    "url": "/css/app.09c0a6a9.css"
+  }
+];
